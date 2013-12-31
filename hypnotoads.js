176e966a4edb3ce03dd39552c9329b9f8db8f939
Borzov <tihoutrom@gmail.com>
2013-12-31 17:03:55 +0600

    game functionality

--- hypnotoads.js
+++ hypnotoads.js
@@ -5,14 +5,17 @@ c.width = 0.95*window.innerWidth;
 
 
 var ctx = c.getContext("2d");
+var newx = 0.5*c.width;
+var newy = 0;
 
-var snowflake_number = 50;
+var snowflake_number = 150;
 var snowflakes = [];
 
 for(var i=0; i< snowflake_number; i++){
   snowflakes.push({
   	x:Math.random()*c.width,
   	y:Math.random()*c.height,
+  	radius:3,
   	wiggle: 3+Math.random()*6,
   	angle: Math.random()*Math.PI*2,
   	tangled: 0
@@ -20,6 +23,7 @@ for(var i=0; i< snowflake_number; i++){
 }
 
 var christmas_tree_number = 180;
+var christmas_tree_indent = 0.4*c.height;
 var christmas_tree_scale = 10;
 var christmas_tree = [];
 
@@ -27,19 +31,15 @@ for(var i=0; i < christmas_tree_number; i++)
 {
 	var position = 0.2*i; 
 	christmas_tree.push({
-		y: 0.4*c.height + christmas_tree_scale*position,
+		y: christmas_tree_indent + christmas_tree_scale*position,
 		x: 0.5* c.width + 0.5*christmas_tree_scale*position*Math.cos(position),
-		mirking: 2+Math.random()*2.,
+		mirking: 1+Math.random()*2.,
 		timer: Math.random()*Math.PI*2
 	})
 }
 
 // a function to update the scene
 function draw(){
-	i++;
-	if (i>c.height) {
-		i=0;
-	}
 	ctx.fillStyle = "rgba(64,83,203,1)";
 	ctx.fillRect(0, 0, c.width, c.height);
 	//caption text
@@ -71,14 +71,32 @@ function draw(){
 		if (snowflakes[j].tangled==0){
 		snowflakes[j].y +=0.8;
 		snowflakes[j].angle += 0.02;
+		if (snowflakes[j].y > christmas_tree_indent && Math.abs(snowflakes[j].x - 0.5* c.width)<400) {
+			parameter = (snowflakes[j].y - christmas_tree_indent)/christmas_tree_scale;
+			if (Math.abs(snowflakes[j].x - 0.5* c.width) < Math.abs(0.5*christmas_tree_scale*parameter*Math.cos(parameter))){
+			    if (Math.random()<0.2){
+			      snowflakes[j].tangled = 2;
+			      snowflakes[j].radius = 7;
+			    }
+		    }
+		}
 	    }
 
 		var p = snowflakes[j];
 		ctx.moveTo(p.x,p.y);
-		ctx.arc(p.x + p.wiggle*Math.cos(p.angle),p.y  + p.wiggle*Math.sin(p.angle),3,0,Math.PI*2,true);
+		ctx.arc(p.x + p.wiggle*Math.cos(p.angle),p.y  + p.wiggle*Math.sin(p.angle),p.radius,0,Math.PI*2,true);
 	if (p.y > c.height) {
-		snowflakes[j].y=0; 
-	}	
+		if (Math.random()<0.3){
+		   snowflakes[j].y=newy;
+		   snowflakes[j].x=newx;
+		   snowflakes[j].radius= 5;
+	    } else {
+           snowflakes[j].y = 0;
+		   snowflakes[j].x = Math.random()*c.width;
+	    }
+	}
+
+
     }
 	ctx.fill()
 }
@@ -86,3 +104,12 @@ function draw(){
 var i = 0;
 console.log("Happy new year to you too, my dear console dwellers! :)")
 setInterval(draw,33);
+
+function onMouseMove(evt) {
+  if (evt.pageX > 0 && evt.pageX < c.width) {
+    newx = evt.pageX;
+    newy = evt.pageY;
+  }
+}
+
+$(document).mousemove(onMouseMove);
